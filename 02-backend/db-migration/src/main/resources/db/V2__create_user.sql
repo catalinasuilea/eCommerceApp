66e2e827da7d4c0cea1f49601e55d83bf893cc33
catalinasuilea <catalinasuilea@yahoo.com>
2022-03-19 19:21:46 +0200

    Create tables.

--- 02-backend/db-migration/src/main/resources/db/V2__create_user.sql
+++ 02-backend/db-migration/src/main/resources/db/V2__create_user.sql
@@ -1,14 +1,33 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS product_category CASCADE;
+DROP TABLE IF EXISTS product CASCADE;
+
 CREATE TABLE IF NOT EXISTS users (
-  user_id serial NOT NULL,
+  user_id serial NOT NULL PRIMARY KEY,
   firstname varchar(255) NOT NULL,
   lastname varchar(255) NOT NULL,
   email varchar(50) NOT NULL,
   phone varchar(20) NOT NULL,
-  user_key varchar(64) DEFAULT NULL,
-  created_date timestamp DEFAULT NULL,
-  modified_date timestamp DEFAULT NULL,
-  created_by int DEFAULT NULL,
-  modified_by int DEFAULT NULL,
-  password varchar(200) DEFAULT NULL,
-  PRIMARY KEY (user_id)
+  created_date timestamp,
+  modified_date timestamp,
+  password varchar(200)
+);
+
+CREATE TABLE IF NOT EXISTS product_category (
+   id SERIAL NOT NULL PRIMARY KEY,
+   category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  unit_price DECIMAL(13,2),
+  image_url VARCHAR(255),
+  units_in_stock INT,
+  created_date timestamp,
+  last_updated_date timestamp,
+  category_id INT NOT NULL,
+
+  FOREIGN KEY (category_id) REFERENCES product_category(id)
 );
