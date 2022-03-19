e0d9139e46a7fab4c33c7f0d59767806bbc0bb40
catalinasuilea <catalinasuilea@yahoo.com>
2022-03-19 21:07:15 +0200

    Configure for ReadOnly

---
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/catalina/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.catalina.ecommerce.config;
+
+import com.catalina.ecommerce.entity.Product;
+import com.catalina.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        //facem ca clasa Product sa fie ReadOnly(se accepta doar metoda GET asupra datelor)
+
+        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        //facem ca clasa ProductCategory sa fie ReadOnly(se accepta doar metoda GET asupra datelor)
+    }
+}
