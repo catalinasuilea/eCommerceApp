509c82bc9bcc9cbe265495db7e427fa63bc59ef4
catalinasuilea <catalinasuilea@yahoo.com>
2022-03-28 20:35:04 +0300

    Add front-end

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    createdDate: Date;
+    lastUpdatedDate: Date;
+}
