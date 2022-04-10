6b3a9f2e19abc752011f82341e6790e9767fc9cd
catalinasuilea <catalinasuilea@yahoo.com>
2022-04-10 22:51:03 +0300

    Search products by categoryId.

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/catalina/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/catalina/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,30 @@ package com.catalina.ecommerce.config;
 
 import com.catalina.ecommerce.entity.Product;
 import com.catalina.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -27,5 +43,27 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
         //facem ca clasa ProductCategory sa fie ReadOnly(se accepta doar metoda GET asupra datelor)
+        
+        // call an internal helper method that help us to expose the ids
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //expose entity ids:
+
+        // step1: get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // step2: create an array list of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity type for the entities
+        for(EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // step3: expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,10 @@
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  //templateUrl: './product-list.component.html',
-  //templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
@@ -13,15 +12,33 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { } //necesar pentru a accesa parametrii unei rute (noi trebuie sa accesam categoryId)
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => { //facem subscribe la parametrii rutei date
+      this.listProducts()
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    
+    //check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id') ///snapshot = state of the route at this given moment in time
+
+    if(hasCategoryId) {
+      //get the "id" param string and convert it to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id'); //paraneter value is returned as a string and we conert it to a number using +
+    }
+    else {
+      //not category id available => default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
