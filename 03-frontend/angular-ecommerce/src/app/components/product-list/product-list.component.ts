86d6a75e96785a16ad6b9a803f78620e4837357c
catalinasuilea <catalinasuilea@yahoo.com>
2022-04-18 00:03:10 +0300

    Add pagination

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -11,9 +11,17 @@ import { ActivatedRoute } from '@angular/router';
 
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
-  currentCategoryId: number;
-  searchMode: boolean;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  //new properties for pagination:
+  thePageNumber: number = 1;
+  thePageSize: number = 4;
+  theTotalElements: number = 0;
+
+  previousKeyword: string = null;
   
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { } //necesar pentru a accesa parametrii unei rute (noi trebuie sa accesam categoryId)
@@ -40,12 +48,19 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts() {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
+    //if we have a different keyword than previous => set thePageNumer = 1
+    if(this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`Keyword=${theKeyword}, thePageNumer=${this.thePageNumber}`);
+
     // now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                               this.thePageSize,
+                                               theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts() {
@@ -60,13 +75,41 @@ export class ProductListComponent implements OnInit {
           //not category id available => default to category id 1
           this.currentCategoryId = 1;
         }
+
+        //Check if we have a different category than previous
+        //Note: angular will reuse a component if it is currently being viewd
+        //if we have a different category id than previous then we need to set thePageNumber back to 1
+        if(this.previousCategoryId != this.currentCategoryId) {
+          this.thePageNumber = 1;
+        }
+
+        this.previousCategoryId = this.currentCategoryId;
+
+        console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
     
         // now get the products for the given category id
-        this.productService.getProductList(this.currentCategoryId).subscribe(
-          data => {
-            this.products = data;
-          }
-        )
+        this.productService.getProductListPaginate(this.thePageNumber - 1, 
+                                                   this.thePageSize,
+                                                   this.currentCategoryId)
+                                                   .subscribe(this.processResult());
+  }
+
+  //We have:
+  //on the left-hand side: properties defined in this class 
+  //over the right-hand side: data that came back from the Spring Data Rest JSON.
+  processResult() {
+    return data => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 
 }
